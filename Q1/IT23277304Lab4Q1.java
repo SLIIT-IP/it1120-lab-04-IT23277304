2eb46aa1becf4cb37ac290a62888185ff34698d4
IT23277304 <140397604+IT23277304@users.noreply.github.com>
2024-08-20 21:37:02 +0530

    IT23277304Lab4Q1

---
+++ Q1/IT23277304Lab4Q1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class IT23277304Lab4Q1{
+public static void main(String[] args){
+Scanner k=new Scanner(System.in);
+System.out.println("enter a number");
+int a=k.nextInt();
+if(a>0)
+System.out.println("the number is :positive");
+else if(a==0)
+System.out.println(" number is zero");
+else
+System.out.println("your number is negative");
+
+
+
+}
+}
